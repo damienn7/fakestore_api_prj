b0b72ed4599960e46121f13c1cb302f84fcef17a
Vtom7 <toms.avenge02@gmail.com>
2025-12-20 15:55:01 +0100

    style(navbar): apply Tailwind styles to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,15 @@
 <template>
-  <header class="flex items-center justify-between px-6 py-4 border-b">
+  <header
+    class="flex items-center justify-between whitespace-nowrap
+           border-b border-slate-200 bg-white px-6 py-4 z-10"
+  >
     <div class="flex items-center gap-4">
-      <div class="size-8 flex items-center justify-center bg-primary text-white rounded-lg">
+      <div class="size-8 flex items-center justify-center bg-primary rounded-lg text-white">
         <span class="material-symbols-outlined">storefront</span>
       </div>
-      <h2 class="text-xl font-bold">ShopMinimal</h2>
+      <h2 class="text-[#0d141c] text-xl font-bold tracking-tight">
+        ShopMinimal
+      </h2>
     </div>
 
     <div class="flex items-center gap-6">
