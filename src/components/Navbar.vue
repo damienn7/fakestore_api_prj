5c97ef09bb401ed463e75df9872f8594ff6d72ab
Vtom7 <toms.avenge02@gmail.com>
2025-12-20 15:54:40 +0100

    feat(navbar): add base navbar layout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,15 @@
 <template>
-  <header>
+  <header class="flex items-center justify-between px-6 py-4 border-b">
+    <div class="flex items-center gap-4">
+      <div class="size-8 flex items-center justify-center bg-primary text-white rounded-lg">
+        <span class="material-symbols-outlined">storefront</span>
+      </div>
+      <h2 class="text-xl font-bold">ShopMinimal</h2>
+    </div>
+
+    <div class="flex items-center gap-6">
+      <!-- actions -->
+    </div>
   </header>
 </template>
 
