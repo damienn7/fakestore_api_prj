96a0a7f89ec02daad02df8f5ff2989ec8c3fcc8c
Aiglator <rayan.chattaoui.com>
2025-12-21 17:23:27 +0100

    feat: integrate useCart in shopfinal modal

--- src/components/shopfinal.vue
+++ src/components/shopfinal.vue
@@ -1,7 +1,7 @@
 <!-- Auteur : Rayan Chattaoui -->
-<!-- Composant panier -->
 <script setup lang="ts">
 import { ref } from "vue";
+import { useCart } from "@/composables/useCart";
 
 const props = defineProps<{
   open: boolean;
@@ -10,13 +10,16 @@ const props = defineProps<{
 const emit = defineEmits<{
   (e: "close"): void;
 }>();
+
+const { isEmpty, itemCount } = useCart();
 </script>
 
 <template>
   <div v-if="open" class="fixed inset-0 z-50">
     <div class="absolute inset-0 bg-black/40" @click="emit('close')" />
-    <div class="relative z-50 w-full max-w-md bg-white h-full ml-auto">
-      <p>Panier</p>
+    <div class="relative z-50 w-full max-w-md bg-white h-full ml-auto p-6">
+      <h2>Panier ({{ itemCount }})</h2>
+      <p v-if="isEmpty">Panier vide</p>
     </div>
   </div>
 </template>
