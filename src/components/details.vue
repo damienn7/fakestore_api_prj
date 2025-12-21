05600951aa3940a504ea62d2b4fbfac06c27a119
Vtom7 <toms.avenge02@gmail.com>
2025-12-21 14:23:17 +0100

    feat:initialisation de la page details

---
+++ src/components/details.vue
@@ -0,0 +1,8 @@
+<!-- Page Détail Produit
+
+    Créer la vue ProductDetail.vue.
+
+    Récupérer les infos via GET /products/{id} en utilisant l'ID de l'URL.
+
+    Afficher la description complète et l'image en grand format. -->
+<!-- Auteur Thomas et Rayan -->
