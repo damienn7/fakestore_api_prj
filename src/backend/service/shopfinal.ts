4fc981747836aa81e64b575a60a267d2d5ecfc13
Aiglator <rayan.chattaoui.com>
2025-12-21 17:16:54 +0100

    feat: implement getallcards method
    premiere version basique

--- src/backend/service/shopfinal.ts
+++ src/backend/service/shopfinal.ts
@@ -4,13 +4,15 @@
 // ensemble des endpoint utilissée
 const API_URL="https://fakestoreapi.com/carts";
 
-import type { Cart } from "../type/cart";
+import type { Cart } from "../type/shop";
 
 /**
- * Récupère la liste complète des produits
+ * Récupère la liste complète des paniers
  */
 export async function getallcards(){
-
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
 }
 export async function addcards(){
 
