6255d173e9d66731fa25778bd43722d187c1c6d4
Aiglator <rayan.chattaoui.com>
2025-12-21 16:57:07 +0100

    feat:initialisation type

---
+++ src/backend/type/shop.ts
@@ -0,0 +1,8 @@
+//Auteur Rayan Chattaoui
+//Interface Cart (version française)
+/* =========================
+   PANIER
+========================= */
+
+import type { ProductFR } from "./products";
+
