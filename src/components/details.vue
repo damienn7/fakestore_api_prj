fc964d0532e0640b701541a940db01edb548c165
Vtom7 <toms.avenge02@gmail.com>
2025-12-21 14:36:52 +0100

    feat(details): fetch product by id from API

--- src/components/details.vue
+++ src/components/details.vue
@@ -1,19 +1,24 @@
-<!-- Page Détail Produit
-
-    Créer la vue ProductDetail.vue.
-
-    Récupérer les infos via GET /products/{id} en utilisant l'ID de l'URL.
-
-    Afficher la description complète et l'image en grand format. -->
-<!-- Auteur Thomas et Rayan -->
+<!-- Auteur : Thomas et Rayan -->
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import { fetchProductById } from "@/backend/service/productapi";
+import type { Product } from "@/backend/type/products";
+
+const route = useRoute();
+const product = ref<Product | null>(null);
+
+onMounted(async () => {
+  const id = Number(route.params.id);
+  product.value = await fetchProductById(id);
+});
 </script>
 
 <template>
   <section class="min-h-screen bg-[#FAFAFA] p-6">
     <div class="max-w-4xl mx-auto bg-white rounded-xl p-6">
-      <h1 class="text-2xl font-bold text-[#0d141c]">
-        Détails du produit
+      <h1 class="text-2xl font-bold text-[#0d141c] mb-4">
+        {{ product?.title }}
       </h1>
     </div>
   </section>
