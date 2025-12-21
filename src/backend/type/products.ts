a2d63e366df25d6695a285959d42d80347044842
Vtom7 <toms.avenge02@gmail.com>
2025-12-21 14:33:53 +0100

    feat:ajout de la logique métier via async function

--- src/backend/type/products.ts
+++ src/backend/type/products.ts
@@ -1,8 +1,11 @@
-export interface types{
-    id: number;
-    titleproduit:string;
-    description:string;
-    prix:number;
-    category:string;
-    image:string;
+// Auteur : Thomas et Rayan
+// Interface Produit (version française)
+
+export interface ProductFR {
+  id: number;
+  titleproduit: string;
+  description: string;
+  prix: number;
+  category: string;
+  image: string;
 }
