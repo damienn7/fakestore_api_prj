c1af0c91e7486aeea9063c89360dd68a3baf777f
Aiglator <rayan.chattaoui.com>
2025-12-21 17:15:58 +0100

    feat: add Product interface
    ajout interface pour FakeStoreAPI

--- src/backend/type/products.ts
+++ src/backend/type/products.ts
@@ -9,3 +9,13 @@ export interface ProductFR {
   category: string;
   image: string;
 }
+
+// interface pour API
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
