21c6443b781245d3d927eee8a05114d440670a91
Aiglator <rayan.chattaoui.com>
2025-12-21 16:50:39 +0100

    feat:initialisation des différentes méthodes

---
+++ src/backend/service/shopfinal.ts
@@ -0,0 +1,29 @@
+// Auteur Rayan Chattaoui
+// Ensemble des méthodes appeller pour cards FakestoreApi
+// version 1.0.0
+// ensemble des endpoint utilissée
+const API_URL="https://fakestoreapi.com/carts";
+
+import type { Cart } from "../type/cart";
+
+/**
+ * Récupère la liste complète des produits
+ */
+export async function getallcards(){
+
+}
+export async function addcards(){
+
+}
+
+export async function getsingle(){
+
+}
+export async function updatecards(){
+    
+}
+export async function deletecards(){
+
+}
+
+
